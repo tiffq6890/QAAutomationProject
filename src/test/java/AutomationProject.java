9de35f808632dd2925a4eeec0f128b51378d20ae
tiffq6890 <tiffq6890@yahoo.com>
2022-03-23 12:53:04 -0500

    Rakshya's Test Scripts

--- src/test/java/AutomationProject.java
+++ src/test/java/AutomationProject.java
@@ -5,14 +5,17 @@
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -84,6 +87,64 @@ public class AutomationProject {
     
     }
     
+    public class SearchBarTestAutomation {
+
+    ChromeDriver driver;
+
+    @BeforeEach
+    public void initializeWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+
+        System.out.println("STEP-1 : Open new browser window.");
+        driver = new ChromeDriver();
+
+        System.out.println("STEP-2 : Enter http://www.target.com in the browser address bar.");
+        driver.get("http://www.target.com");
+    }
+   
+    @Test
+    public void testSearchBarUsingProductName() {
+        System.out.println("STEP-3 : Enter â€œiphone 13 proâ€ in the search bar");
+        WebElement searchBar = driver.findElement(By.id("search"));
+        searchBar.sendKeys("iphone 13 pro");
+        System.out.println("STEP-4 : Click search icon");
+        WebElement searchIcon = driver.findElement(By.className("SearchButtons__SearchButton-sc-sj12sv-1"));
+        searchIcon.click();
+        //wait for the elements to load
+        WebDriverWait driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@aria-label='Apple iPhone 13 Pro']")));
+        System.out.println("STEP-5 :Verify page shows results containing at least one iphone 13 pro");
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@aria-label='Apple iPhone 13 Pro']"));
+        Assertions.assertTrue(elements.size() > 0, "No result shown that matches search");
+    }
+    
+     @Test
+    public void testSearchBarUsingProductUPC() {
+
+        System.out.println("STEP-3 : Enter UPC Number in search bar");
+        WebElement searchBar = driver.findElement(By.id("search"));
+        searchBar.sendKeys("194253118831");
+        System.out.println("STEP-4 : Click search icon");
+        WebElement searchIcon = driver.findElement(By.className("SearchButtons__SearchButton-sc-sj12sv-1"));
+        searchIcon.click();
+        System.out.println("STEP-5 Verify that search result shows a product matching UPC number");
+        //wait for the elements to load
+        WebDriverWait driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@aria-label='Apple iPhone 13 Pro']")));
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@aria-label='Apple iPhone 13 Pro']"));
+        Assertions.assertTrue(elements.size() == 1, "No result shown that matches search");
+
+    }
+
+    
+
+    @AfterEach
+    public void cleanWebdriver() {
+        driver.close();
+    }
+
+}
+
    
         
     }
